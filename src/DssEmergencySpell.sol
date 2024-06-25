50493ac34c39006c79daf005b0a4f06092161f95
amusingaxl <112016538+amusingaxl@users.noreply.github.com>
2024-06-25 18:04:09 -0300

    refactor(base-emergency-spell): change tag to `extcodehash(address(this))`

--- src/DssEmergencySpell.sol
+++ src/DssEmergencySpell.sol
@@ -19,7 +19,7 @@ interface ChainlogLike {
     function getAddress(bytes32 key) external view returns (address);
 }
 
-interface DssEmergencySpellLike {
+interface DssExec {
     function action() external view returns (address);
     function cast() external;
     function description() external view returns (string memory);
@@ -35,29 +35,77 @@ interface DssEmergencySpellLike {
     function tag() external view returns (bytes32);
 }
 
+interface DssAction {
+    function actions() external;
+    function description() external view returns (string memory);
+    function execute() external;
+    function nextCastTime(uint256 eta) external view returns (uint256);
+    function officeHours() external view returns (bool);
+}
+
+interface DssEmergencySpellLike is DssExec, DssAction {
+    function description() external view override(DssExec, DssAction) returns (string memory);
+    function officeHours() external view override(DssExec, DssAction) returns (bool);
+}
+
 abstract contract DssEmergencySpell is DssEmergencySpellLike {
+    /// @dev The chainlog contract reference.
     ChainlogLike internal constant _log = ChainlogLike(0xdA0Ab1e0017DEbCd72Be8599041a2aa3bA7e740F);
 
+    /// @dev The chainlog address.
     address public constant log = address(_log);
+    /// @dev In regular spells, `eta` is used to enforce the GSM delay.
+    ///      For emergency spells, the GSM delay is not applicable.
     uint256 public constant eta = 0;
-    bytes public constant sig = "";
+    /// @dev Keeping the same value as regular spells.
+    bytes public constant sig = abi.encodeWithSelector(DssAction.execute.selector);
+    /// @dev Emergency spells should not expire.
     uint256 public constant expiration = type(uint256).max;
-    // @notice Office Hours is always `false` for emergency spells.
+    // @dev Office Hours is always `false` for emergency spells.
     bool public constant officeHours = false;
+    // @dev `action` is expected to return a valid address.
+    //         We also implement the `DssAction` interface in this contract.
     address public immutable action = address(this);
-    bytes32 public immutable tag = keccak256(abi.encodePacked(address(this)));
+    // @dev The reference to the `pause` contract.
     address public immutable pause = ChainlogLike(log).getAddress("MCD_PAUSE");
-    uint256 public immutable nextCastTime = block.timestamp;
+    // @dev An emergency spell can be cast as soon as it is deployed.
+    //      Notice that cast is always a no-op.
+    uint256 internal immutable _nextCastTime = block.timestamp;
 
+    /**
+     * @dev In regular spells, `tag` is an immutable variable with the code hash of the spell action.
+     *      It specifically uses a separate contract for spell action because `tag` is immutable and the code hash of
+     *      the contract being initialized is not accessible in the constructor.
+     *      Since we do not have a separate contract for actions in Emergency Spells, `tag` has to be turned into a
+     *      getter function instaed of an immutable variable.
+     */
+    function tag() external view returns (bytes32 _tag) {
+        assembly {
+            _tag := extcodehash(address())
+        }
+    }
 
     /**
      * @notice Triggers the emergency actions of the spell.
      * @dev Emergency spells are triggered when scheduled.
      *      This function maintains the name for compatibility with regular spells, however nothing is actually being
-     *      scheduled. Emergency spell take affect immediately, so there is no need to call `pause.plot()`.
+     *      scheduled. Emergency spells take affect immediately, so there is no need to call `pause.plot()`.
      */
     function schedule() external {
         _emergencyActions();
+    }
+
+    /**
+     * @notice Implements the emergency actions to be triggered by the spell.
+     */
+    function _emergencyActions() internal virtual;
+
+    /**
+     * @notice Returns `_nextCastTime`.
+     * @dev this function exists only to keep interface compatibility with regular spells.
+     */
+    function nextCastTime() external view returns (uint256 castTime) {
+        return _nextCastTime;
     }
 
     /**
@@ -67,8 +115,22 @@ abstract contract DssEmergencySpell is DssEmergencySpellLike {
     function cast() external {}
 
     /**
-     * @notice Implements the emergency actions to be triggered by the spell.
+     * @notice No-op.
+     * @dev this function exists only to keep interface compatibility with regular spells.
      */
-    function _emergencyActions() internal virtual;
+    function execute() external {}
 
+    /**
+     * @notice No-op.
+     * @dev this function exists only to keep interface compatibility with regular spells.
+     */
+    function actions() external {}
+
+    /**
+     * @notice Returns `nextCastTime`, regardless of the input parameter.
+     * @dev this function exists only to keep interface compatibility with regular spells.
+     */
+    function nextCastTime(uint256) external view returns (uint256 castTime) {
+        return _nextCastTime;
+    }
 }

549b45739681e52322897be062544c39fd244fae
oddaf <106770775+oddaf@users.noreply.github.com>
2024-10-01 01:29:47 -0300

    feat: Splitter disable emergency Spell

---
+++ src/splitter-stop/SplitterStopSpell.sol
@@ -0,0 +1,51 @@
+// SPDX-FileCopyrightText: © 2024 Dai Foundation <www.daifoundation.org>
+// SPDX-License-Identifier: AGPL-3.0-or-later
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+pragma solidity ^0.8.16;
+
+import {DssEmergencySpell} from "../DssEmergencySpell.sol";
+
+interface SplitterMomLike {
+    function stop() external;
+}
+
+interface SplitterLike {
+    function hop() external view returns (uint256);
+}
+
+contract SplitterStopSpell is DssEmergencySpell {
+    string public constant override description = "Emergency Spell | Disable Splitter";
+
+    SplitterMomLike public immutable splitterMom = SplitterMomLike(_log.getAddress("SPLITTER_MOM"));
+    SplitterLike public immutable splitter = SplitterLike(_log.getAddress("MCD_SPLIT"));
+
+    event SplitterDisabled();
+
+    /**
+     * @notice Disables Splitter
+     */
+    function _emergencyActions() internal override {
+        splitterMom.stop();
+        emit SplitterDisabled();
+    }
+
+    /**
+     * @notice Returns whether the spell is done or not.
+     * @dev Checks if `splitter.hop() == type(uint).max` (disabled).
+     */
+    function done() external view returns (bool) {
+        return splitter.hop() == type(uint256).max;
+    }
+}
---
+++ src/splitter-stop/SplitterStopSpell.t.integration.sol
@@ -0,0 +1,62 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+pragma solidity ^0.8.16;
+
+import {stdStorage, StdStorage} from "forge-std/Test.sol";
+import {DssTest, DssInstance, MCD, GodMode} from "dss-test/DssTest.sol";
+import {SplitterStopSpell, SplitterLike} from "./SplitterStopSpell.sol";
+
+contract SplitterStopSpellTest is DssTest {
+    using stdStorage for StdStorage;
+
+    address constant CHAINLOG = 0xdA0Ab1e0017DEbCd72Be8599041a2aa3bA7e740F;
+    DssInstance dss;
+    address chief;
+    SplitterLike splitter;
+    SplitterStopSpell spell;
+
+    function setUp() public {
+        vm.createSelectFork("mainnet");
+
+        dss = MCD.loadFromChainlog(CHAINLOG);
+        MCD.giveAdminAccess(dss);
+        chief = dss.chainlog.getAddress("MCD_ADM");
+        splitter = SplitterLike(dss.chainlog.getAddress("MCD_SPLIT"));
+        spell = new SplitterStopSpell();
+
+        stdstore.target(chief).sig("hat()").checked_write(address(spell));
+
+        vm.makePersistent(chief);
+    }
+
+    function testSplitterStopOnSchedule() public {
+        uint256 preHop = splitter.hop();
+        assertTrue(preHop != type(uint256).max, "before: Splitter already stopped");
+        assertFalse(spell.done(), "before: spell already done");
+
+        vm.expectEmit(true, false, false, false, address(spell));
+        emit SplitterDisabled();
+
+        spell.schedule();
+
+        uint256 postHop = splitter.hop();
+        assertEq(postHop, type(uint256).max, "after: Splitter not stopped");
+        assertTrue(spell.done(), "after: spell not done");
+    }
+
+    function testRevertSplitterStopWhenItDoesNotHaveTheHat() public {
+        stdstore.target(chief).sig("hat()").checked_write(address(0));
+
+        uint256 preHop = splitter.hop();
+        assertTrue(preHop != type(uint256).max, "before: Splitter already stopped");
+        assertFalse(spell.done(), "before: spell already done");
+
+        vm.expectRevert();
+        spell.schedule();
+
+        uint256 postHop = splitter.hop();
+        assertEq(postHop, preHop, "after: Splitter stopped unexpectedly");
+        assertFalse(spell.done(), "after: spell done unexpectedly");
+    }
+
+    event SplitterDisabled();
+}

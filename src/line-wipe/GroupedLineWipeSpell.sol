0c88f6a6020594940058ab86530415109b1b7de3
amusingaxl <112016538+amusingaxl@users.noreply.github.com>
2025-02-11 14:31:43 -0300

    fix: amend broken license headers

--- src/line-wipe/GroupedLineWipeSpell.sol
+++ src/line-wipe/GroupedLineWipeSpell.sol
@@ -1,3 +1,8 @@
+// SPDX-FileCopyrightText: © 2024 Dai Foundation <www.daifoundation.org>
+// SPDX-License-Identifier: AGPL-3.0-or-later
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
